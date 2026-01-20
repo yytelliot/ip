73adb58b0ec66d7f65207d531a6510d8a7ab16be
yytelliot <yytelliot@gmail.com>
2026-01-21 01:39:23 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Pookie.java
@@ -0,0 +1,13 @@
+public class Pookie {
+    private static final String LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+
+        System.out.println(LINE);
+        System.out.println(" Hello! I'm Pookie! x3");
+        System.out.println(" What can I do for you? OwO\n");
+        System.out.println(LINE);
+        System.out.println(" See you again soon! UwU");
+        System.out.println(LINE);
+    }
+}
