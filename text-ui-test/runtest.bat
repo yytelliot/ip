e7b08e98c4023ebf37f16dc2f78e478478d9622a
yytelliot <yytelliot@gmail.com>
2026-01-22 16:37:23 +0800

    Added unit tests
    Added semi automated text UI testing for pookie and updated the runtest scripts
    for windows and unix. Added sample input and EXPECTED output.

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+SETLOCAL
 
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
@@ -6,16 +7,24 @@ if not exist ..\bin mkdir ..\bin
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
-REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+REM collect all java files under src/main/java into SOURCES.TXT
+dir /s /b ..\src\main\java\*.java > SOURCES.TXT
+
+REM compile everything
+javac -Xlint:none -d ..\bin -sourcepath ..\src\main\java @SOURCES.TXT
+
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
+    del SOURCES.TXT
     exit /b 1
 )
 REM no error here, errorlevel == 0
 
+REM clean up temporary file
+del SOURCES.TXT
+
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
+java -classpath ..\bin pookie.Pookie < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -9,18 +9,25 @@ fi
 # delete output from previous run
 if [ -e "./ACTUAL.TXT" ]
 then
-    rm ACTUAL.TXT
+    rm -f ACTUAL.TXT SOURCES.TXT EXPECTED-UNIX.TXT
 fi
+
+# collect all java files under src/main/java into SOURCES.TXT
+find ../src/main/java -name "*.java" > SOURCES.TXT
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -Xlint:none -d ../bin -sourcepath ../src/main/java @SOURCES.TXT
 then
-    echo "********** BUILD FAILURE **********"
-    exit 1
+  echo "********** BUILD FAILURE **********"
+  rm -f SOURCES.TXT
+  exit 1
 fi
 
-# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+# clean up temporary file
+rm -f SOURCES.TXT
+
+# run the program, feed commands from input.txt file and redirect the output to ACTUAL.TXT
+java -classpath ../bin pookie.Pookie < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
