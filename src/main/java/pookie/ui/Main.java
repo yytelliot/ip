e2606d2dc7748498128b76fc7fa3e4d96fcb2ad5
yytelliot <yytelliot@gmail.com>
2026-02-17 22:48:51 +0800

    Add window name

--- src/main/java/pookie/ui/Main.java
+++ src/main/java/pookie/ui/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Pookie");
             fxmlLoader.<MainWindow>getController().setPookie(pookie);  // inject the Pookie instance
             stage.show();
         } catch (IOException e) {
