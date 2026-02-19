ed05be808e3c389ec02d3e08463466b1d0edaa69
yytelliot <yytelliot@gmail.com>
2026-02-19 18:08:15 +0800

    Refactor: Simplify MarkCommand and UnmarkCommand using batch operations
    MarkCommand and UnmarkCommand have duplicate logic: parse indices, fetch
    tasks, apply an operation, save, and format response.
    
    Reducing duplication makes the code easier to maintain and extend.
    
    Let's extract the common batch operation pattern into a reusable
    executeBatchOperation method. MarkCommand and UnmarkCommand now delegate
    to this method with lambdas specifying their specific operation.

--- src/main/java/pookie/command/UnmarkCommand.java
+++ src/main/java/pookie/command/UnmarkCommand.java
@@ -1,10 +1,7 @@
 package pookie.command;
 
-import java.util.List;
-
 import pookie.exception.PookieException;
 import pookie.storage.Storage;
-import pookie.task.Task;
 import pookie.task.TaskList;
 
 /**
@@ -20,24 +17,9 @@ public class UnmarkCommand extends Command {
 
     @Override
     public String execute(TaskList taskList, Storage storage) throws PookieException {
-        if (args.length < MIN_INDEX_COMMAND_ARGS) {
-            throw new PookieException("Please provide the index of the task to unmark! >w<");
-        }
-
-        String[] indicesArgs = new String[args.length - 1];
-        System.arraycopy(args, 1, indicesArgs, 0, args.length - 1);
-
-        List<Integer> indices = parseTaskIndices(taskList, indicesArgs);
-        List<Task> tasks = getTasksByIndices(taskList, indices);
-
-        for (Task task : tasks) {
-            task.markAsUndone();
-        }
-        saveTaskList(taskList, storage);
-
-        String header = indices.size() == 1
-                ? "OK, I've marked this task as not done yet ;w;"
-                : "OK, I've marked these tasks as not done yet ;w;";
-        return formatTasksWithIndices(header, indices, tasks);
+        return executeBatchOperation(args, taskList, storage,
+                task -> task.markAsUndone(),
+                "OK, I've marked this task as not done yet ;w;",
+                "OK, I've marked these tasks as not done yet ;w;");
     }
 }
